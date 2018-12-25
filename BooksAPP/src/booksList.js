057d7593ea1c8795b022d7bc38979a45c886081c
wishesBAO <39586017+wishesBAO@users.noreply.github.com>
2018-12-25 16:55:16 +0800

    Update booksList.js

--- BooksAPP/src/booksList.js
+++ BooksAPP/src/booksList.js
@@ -15,20 +15,23 @@ class BooksList extends Component {
     		return (
 				<ol className="books-grid">
       				{this.props.books.map((book) => (
-     			 	<li key={ book.title }>
+     			 	<li key={ book.id }>
 						<div className="book">
 							<div className="book-top">
 								<div className="book-cover" style={{ width: 130, height: 190, backgroundImage:(book.imageLinks ? 'url('+book.imageLinks.smallThumbnail+')' :'url(https://books.google.com/googlebooks/images/no_cover_thumb.gif)' ) }}></div>
 									<div className="book-shelf-changer">
-											<select 
-							onChange={(e)=>this.props.changeStatus(e.target.value)} 
-						value={ book.shelf + book.id }>
-												<option value={"undefined"+book.id} disabled >放到哪呢...</option>
-												<option value={"currentlyReading"+book.id}>正在阅读</option>
-												<option value={"wantToRead"+book.id}>想要阅读</option>
-												<option value={"read"+book.id}>已读完了</option>
-												<option value={"del"+book.id}>拜拜了您嘞</option>
-											</select>
+<select defaultValue={ book.shelf } onChange={(e) => { /* defaultValue 用来设置每本书的默认状态值 */
+									let newShelf = e.target.value /* 当状态改变时获取选中项value */
+         							if (book.shelf !== newShelf) { /* 一旦发现改变的值不与先前的相同就触发 updateBook 方法 */
+           							this.props.updateBook(book, newShelf);/* 将当前图书对象以及新的选中项状态值传递过去 */
+        							 }
+       								}}>
+         							<option value="none" disabled>Move to...</option>
+         							<option value="currentlyReading">Currently Reading</option>
+         							<option value="wantToRead">Want to Read</option>
+         							<option value="read">Read</option>
+         							<option value="none">None</option>
+       						</select>
 									</div>
 							</div>
 							<div className="book-title">{ book.title }</div>
